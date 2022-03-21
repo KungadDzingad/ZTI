ac077ff4479ed8878f57a661bfee874ff7d24889
Wojciech Wróbel <wojciechwrobel22@gmail.com>
2022-03-21 08:13:58 +0100

    pierwszy commit

---
+++ src/main/java/pl/karinawojtek/ztiserver/ZtiServerApplication.java
@@ -0,0 +1,13 @@
+package pl.karinawojtek.ztiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/karinawojtek/ztiserver/ZtiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karinawojtek.ztiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
